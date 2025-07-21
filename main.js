d1aab0ccdb7f7d9f54f49d28fdee66e1ca2d3d32
Geraldine <Geraldinerevilla21@gmail.com>
2025-07-21 16:06:47 -0500

    commit cambios guardados

--- main.js
+++ main.js
@@ -23,10 +23,23 @@ const productos = [
   { nombre: "Destornillador", categoria: "Ferretería", precio: 30 },
   { nombre: "Cinta métrica", categoria: "Ferretería", precio: 25 },
 ];
-for (let i  = 0; i < productos.length; i++) {
-  const element= productos[i];
-  if(element.categoria==="Ropa"){//si cambia la categoria a va a imprimir esa categoria especifica
-    console.log(productos[i])
+
+let contenedor = document.querySelector("#contenedor-productos");
+
+for (let i = 0; i < productos.length; i++) {
+  const element = productos[i];
+  if (element.categoria === "Ferretería") {
+    //si cambia la categoria a va a imprimir esa categoria especifica
+    contenedor.innerHTML += `
+            <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-amber-900 dark:text-white">${productos[i].nombre}</h5>
+                <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                <p class="font-normal text-gray-700 dark:text-gray-400">Categoria: ${productos[i].categoria}</p>
+                <p class="font-normal text-gray-700 dark:text-gray-400">Precio: ${productos[i].precio} $</p>
+            </a>
+            `;
   }
-  
 }
